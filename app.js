5f20c8e4f5b49b29f7b1ced768ba6e4f5c9bf0b3
Jamshid <kalandarovjamshid01@gmail.com>
2022-07-11 11:07:59 +0500

    routes

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require("express");
 
 const app = express();
+//Static Files
+app.use(express.static("public"))
+app.use("/css",express.static(__dirname+"public/css"))
+app.use("/img",express.static(__dirname+"public/css"))
+app.use("/js",express.static(__dirname+"public/css"))
+
+//Templating Engine
+app.set("views","./src/views")
+app.set("view engine","ejs")
+
+//Routes
+
+const newsRouter=require("./src/routes/news")
+app.use("/",newsRouter)
+
 
 const port = 5000;
 app.listen(port, () => {
