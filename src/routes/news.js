1ad2a716474acad85e106ea61165032df600bf7a
Jamshid <kalandarovjamshid01@gmail.com>
2022-07-11 18:29:28 +0500

    iwladi

--- src/routes/news.js
+++ src/routes/news.js
@@ -1,8 +1,26 @@
 const express=require("express");
 const newsRouter=express.Router();
 
+const axios=require("axios")
+
+
 newsRouter.get("",async(req,res)=>{
-    res.render("news")
+try {
+    const newsApi=await axios.get("https://raddy.dev/wp-json/wp/v2/posts")
+    res.render("news",{ articles:newsApi.data})
+    console.log(newsApi)
+} catch (error) {
+ if(error.response){
+    console.log(error.response.data)
+    console.log(error.response.status)
+    console.log(error.response.headers)
+ }else if(error.request){
+    console.log(error.request)
+ }else{
+    console.log("ERROR",err.message)
+ }
+}
+
 });
 
 
