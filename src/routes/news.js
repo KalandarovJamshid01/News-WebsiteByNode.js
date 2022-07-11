8669bf0e2f5b78f2792aa89f6381aa6605971836
Jamshid <kalandarovjamshid01@gmail.com>
2022-07-12 02:32:56 +0500

    search is created

--- src/routes/news.js
+++ src/routes/news.js
@@ -4,7 +4,7 @@ const newsRouter=express.Router();
 const axios=require("axios")
 
 
-newsRouter.get("",async(req,res)=>{
+newsRouter.get("/",async(req,res)=>{
 try {
     const newsApi=await axios.get("https://raddy.dev/wp-json/wp/v2/posts")
     res.render("news",{ articles : newsApi.data })
@@ -55,4 +55,30 @@ newsRouter.get("/:id",async(req,res)=>{
    }
    
    });
+   newsRouter.post("",async(req,res)=>{
+
+      let search=req.body.search
+      try {
+          const newsApi=await axios.get(`https://raddy.dev/wp-json/wp/v2/posts?search=${search}`)
+          res.render("newsSearch",{ articles : newsApi.data })
+      } catch (error) {
+       if(error.response){
+         res.render("newsSearch",{ articles : null })
+      
+          console.log(error.response.data)
+          console.log(error.response.status)
+          console.log(error.response.headers)
+       }else if(error.request){
+         res.render("newsSearch",{ articles: null })
+      
+          console.log(error.request)
+         res.render("newsSearch",{ articles : null })
+       }else{
+         res.render("newsSearch",{ articles : null })
+      
+          console.log("ERROR",err.message)
+       }
+      }
+      
+      });
 module.exports=newsRouter;
